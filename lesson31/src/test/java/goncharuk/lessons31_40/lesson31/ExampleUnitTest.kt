4fad5e5a198f7ac989c35b19a4378bc1f8300de7
Pavel <pavlusius@gmail.com>
2019-12-04 07:42:00 +0300

    main init

---
+++ lesson31/src/test/java/goncharuk/lessons31_40/lesson31/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package goncharuk.lessons31_40.lesson31
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
